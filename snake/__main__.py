c435cec82e047e493662ec58aebda2c7ed556866
janaSunrise <warriordefenderz@gmail.com>
2021-04-09 13:26:53 +0530

    Abstract the functions into a game class.

--- snake/__main__.py
+++ snake/__main__.py
@@ -6,52 +6,67 @@ from .food import Food
 from .snake import Snake
 from .utils import draw_grid
 
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.font = pygame.font.SysFont("monospace", 16)
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+
+        pygame.display.set_caption("Snake game python")
+
+        self.surface = pygame.Surface(self.screen.get_size())
+        self.surface = self.surface.convert()
+
+        self.snake = Snake(self.screen)
+        self.food = Food()
+
+        self.high_score = 0
+
+        # Draw the grid
+        draw_grid(self.surface, GRID_SIZE, GRID_HEIGHT, GRID_WIDTH)
+
+    def update_score(self):
+        self.score = self.font.render(f"Score: {self.snake.score}", True, Colors.WHITE)
+        self.high_score_ = self.font.render(f"High Score: {self.high_score}", True, Colors.WHITE)
+
+        self.screen.blit(self.score, (5, 10))
+        self.screen.blit(self.high_score_, (5, 26))
+
+    def redraw_graphics(self):
+        self.clock.tick(self.snake.snake_speed)
+        self.snake.handle_keys()
+        draw_grid(self.surface, GRID_SIZE, GRID_HEIGHT, GRID_WIDTH)
+
+        self.snake.move()
+
+        if self.snake.get_head_position() == self.food.position:
+            if self.snake.score % 2 == 0:
+                self.snake.snake_speed += 1
+
+            self.snake.length += 1
+            self.snake.score += 1
+
+            self.food.randomize_position()
+
+            if self.snake.score > self.high_score:
+                self.high_score = self.snake.score
+
+        self.snake.draw(self.surface)
+        self.food.draw(self.surface)
+        self.screen.blit(self.surface, (0, 0))
+
+    def update_screen(self):
+        while True:
+            self.redraw_graphics()
+            self.update_score()
+
+            pygame.display.update()
+
+
 if __name__ == '__main__':
-    pygame.init()
-
-    font = pygame.font.SysFont("monospace", 16)
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-
-    pygame.display.set_caption("Snake game python")
-
-    surface = pygame.Surface(screen.get_size())
-    surface = surface.convert()
-    draw_grid(surface, GRID_SIZE, GRID_HEIGHT, GRID_WIDTH)
-
-    snake = Snake(screen)
-    food = Food()
-
-    high_score = 0
-
-    while True:
-        clock.tick(snake.snake_speed)
-        snake.handle_keys()
-        draw_grid(surface, GRID_SIZE, GRID_HEIGHT, GRID_WIDTH)
-
-        snake.move()
-
-        if snake.get_head_position() == food.position:
-            if snake.score % 2 == 0:
-                snake.snake_speed += 1
-
-            snake.length += 1
-            snake.score += 1
-
-            food.randomize_position()
-
-            if snake.score > high_score:
-                high_score = snake.score
-
-        snake.draw(surface)
-        food.draw(surface)
-        screen.blit(surface, (0, 0))
-
-        score = font.render(f"Score: {snake.score}", True, Colors.WHITE)
-        high_score_ = font.render(f"High Score: {high_score}", True, Colors.WHITE)
-
-        screen.blit(score, (5, 10))
-        screen.blit(high_score_, (5, 26))
-
-        pygame.display.update()
+    game = Game()
+    game.update_screen()
 
